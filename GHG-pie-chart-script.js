3df8ebc1dd9de9eea61a91d563c442507759d442
ddang <ddang@pop-os.localdomain>
2023-03-01 03:21:57 -0500

    finished main-active.html

--- GHG-pie-chart-script.js
+++ GHG-pie-chart-script.js
@@ -22,8 +22,17 @@ function rgbToHex(r, g, b)
 
 function colorChange(hex, deltaR, deltaG, deltaB)
 {
+    function gate(c){
+        if (c > 255){
+            return 255;
+        }
+        else if (c < 0){
+            return 0;
+        }
+        else return c;
+    }
     var lightColor = hexToRgb(hex);
-    return rgbToHex(lightColor.r + deltaR, lightColor.g + deltaG, lightColor.b + deltaB);
+    return rgbToHex(gate(lightColor.r + deltaR), gate(lightColor.g + deltaG), gate(lightColor.b + deltaB));
 }
 
 function colorLighten(hex)
@@ -31,22 +40,64 @@ function colorLighten(hex)
     return colorChange(hex, 25, 25, 25);
 }
 
-/* Main functions */
-var main_pie_linked = document.getElementById("sec_slice_label_path");
-main_pie_linked.onmousenter = () =>
+const svgNS = "http://www.w3.org/2000/svg";
+const svg = document.getElementsByTagName("svg")[0];
+const defs = document.getElementsByTagName("defs")[0];
+
+function getFill(id)
 {
-    var original_color = main_pie_linked.style.fill;
-    main_pie_linked.style.fill = colorLighten(original_color);
+	let element = document.getElementById(id);
+    var fill;
+	if (element.currentStyle) {
+		fill = x.currentStyle["fill"];
+    }
+	else if (window.getComputedStyle) {
+		fill = getComputedStyle(element).getPropertyValue("fill");
+    }
+    var rgb = fill.split(",");
+    return rgbToHex(parseInt(rgb[0].substring(4)), parseInt(rgb[1]), parseInt(rgb[2]));
 }
 
-var ag_main_pie = document.getElementById("agriculture_main_pie");
-var poop_sec_slice = document.getElementById("livestock_manure");
-var hover_shadow = document.getElementById("wasteHoverShadow");
+function createDropShadow(id, idSuffix){
+    let filter = document.createElementNS(svgNS, "filter");
+    filter.setAttribute("id", id + "_filter")
+    let feDropShadow = document.createElementNS(svgNS, "feDropShadow");
+    feDropShadow.setAttribute("dx", "0");
+    feDropShadow.setAttribute("dy", "0");
+    feDropShadow.setAttribute("stdDeviation", "10");
+    feDropShadow.setAttribute("flood-opacity", "0.7");
+    feDropShadow.setAttribute("flood-color", colorLighten(getFill(id + idSuffix)));
+    filter.appendChild(feDropShadow);
+    defs.appendChild(filter);
+}
 
-ag_main_pie.onmouseenter = () => {
-    poop_sec_slice.style.fill = "red";
-    poop_sec_slice.style.color = "red";
-    document.documentElement.style.setProperty('--test-color', '#FC2B0A');
-    poop_sec_slice.style.filter = "url(#wasteHoverShadow)";
-};
-ag_main_pie.onmouseleave = () => { poop_sec_slice.style.fill = "green"; poop_sec_slice.style.color = "green" };
+function putOnTop(id){
+    let element = document.getElementById(id).parentNode;
+    element.parentNode.appendChild(element);
+}
+
+function hoverEffect(ids, idSuffix)
+{
+    ids.forEach(id => {
+        let element = document.getElementById(id);
+        let elementSuffixed = document.getElementById(id + idSuffix);
+        elementSuffixed.style.stroke = "#FFFFFF";
+        element.style.zIndex = "0";
+        let filterId = id + "_filter";
+        element.onmouseenter = () => {
+            createDropShadow(id, idSuffix);
+            element.style.filter = "url(#" + filterId + ")";
+            elementSuffixed.style.strokeWidth = "2";
+            putOnTop(id);
+        };
+        element.onmouseleave = () => {
+            element.style.filter = "";
+            elementSuffixed.style.strokeWidth = "0";
+            defs.removeChild(document.getElementById(filterId));
+    }});   
+}
+
+
+/* Main functions */
+const main_pie_array = ["agriculture", "waste", "industry", "energy"].map(x => x + "_main_pie");
+hoverEffect(main_pie_array, "_path");
